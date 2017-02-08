2a3150f8f10e8adb49b231381e3cac63c8fda198
Baptiste <Baptiste@Samsung>
2017-02-08 18:38:39 +0100

    Configure project (FXML, packages, controllers).

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,17 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("/view/StockURL.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
---
+++ src/controller/StockURLController.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author Baptiste
+ *
+ */
+public class StockURLController {
+
+}
